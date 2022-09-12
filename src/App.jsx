0cae753cad2cad9e1d084c245f80736c08509494
kratos1710 <sbhandari1907@protonmail.com>
2022-09-12 13:59:51 +0530

    config changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './Pages/Home'
-import CategoryCard from './Components/HomeComponents/Common components/CategoryCard'
-import ProductCard from './Components/HomeComponents/Common components/ProductCard'
-import Login from './Pages/Login'
-import SignUp from './Pages/SignUp'
-import Product from './Pages/Product'
+import { useState } from "react";
+import "./App.css";
+import Home from "./Pages/Home";
+import CategoryCard from "./Components/HomeComponents/Common components/CategoryCard";
+import ProductCard from "./Components/HomeComponents/Common components/ProductCard";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import Product from "./Pages/Product";
 
 function App() {
-
   return (
     <>
-      {/* <Home/> */}
-      {/* <Login/> */}
-      {/* <SignUp/> */}
-      <Product/>
-      {/* <CategoryCard/>
-      <ProductCard/> */}
+      {/* <Home /> */}
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+      <Product />
+      {/* <CategoryCard /> */}
+      {/* <ProductCard /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
