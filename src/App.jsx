57b67a5b3784f6d794fcfef6105b480d667b7e17
Anshika <anshikathakur596@gmail.com>
2022-09-29 22:16:10 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
+    
       {/* <Product /> */}
       {/* <CategoryCard /> */}
       {/* <ProductCard /> */}
